555a1a765eadaf5b2e4609e6561eb17625bb3233
FredGuiou <frederic.guiou@gmail.com>
2022-05-17 14:28:38 +0200

    Etape 2 Ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,18 +4,30 @@ const app = {
 
   addListenerToActions(){
     document.getElementById("addListButton").addEventListener("click", app.showAddListModal);
+    
+    const buttons = document.querySelectorAll(".modal");
+    
+    buttons.forEach(button => {
+    button.addEventListener("click", app.hideModals);
+    });
+    
+
   },
 
   showAddListModal(){
     document.getElementById("addListModal").classList.add('is-active');
+  },
+
+  hideModals(){
+    document.getElementById("addListModal").classList.remove('is-active');
   }
 
 };
 
 function init () {
-  console.log('app.init !');
+  console.log("Welcome To O'kanban !");
   app.addListenerToActions();
-}
+};
 
 init();
 
